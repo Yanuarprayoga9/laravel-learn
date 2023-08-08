e014dd4bfc7cac8deffc1923ce194f0c33cfd5f2
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-08-08 09:57:06 +0700

    add details

---
+++ resources/views/student/students.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.components.MainLayout')
+@section('title','Home')
+@section('content')
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>no</th>
+            <th>nama</th>
+            <th>Nis</th>
+            <th>gender</th>
+            <th>opsi</th>
+            {{-- <th>Kelas id</th>
+            <th>Kelas</th>
+            <th>Extra</th>
+            <th>walikelas</th> --}}
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($studentList as $item)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $item['name'] }}</td>
+            <td>{{ $item['nis'] }}</td>
+            <td>{{ $item['gender'] }}</td>
+            {{-- <td>{{ $item['class_id'] }}</td>
+            <td>{{ $item->class['name'] }}</td>
+            <td>
+                 @foreach ($item->extracurriculars as $extracurricular)
+                    {{ $extracurricular->name }} <br>
+                @endforeach    
+            </td>
+            <td>{{ $item->class->homeroomTeacher->name }}</td> --}}
+            <td>
+                <a href="student/{{ $item->id }}" class="btn btn-warning">
+                    detail
+                </a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+
+</table>
+@endsection
--- resources/views/student/teacher.blade.php
+++ resources/views/student/teacher.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.components.MainLayout')
 @section('title', 'class')
 @section('content')
-    <h1>ini adalah page class</h1>
+    <h1>ini adalah page walikelas</h1>
 
     <table class="table table-bordered">
         <thead>
