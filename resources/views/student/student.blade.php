581e8ed3d3f426c19cce37d3c016565364493e11
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-08-08 10:30:26 +0700

    detail

--- resources/views/student/student.blade.php
+++ resources/views/student/student.blade.php
@@ -1,32 +1,28 @@
 @extends('layouts.components.MainLayout')
 @section('title', 'Home')
 @section('content')
-
-
     halo ini detail student <br>
-    <ol>
-        <li>
-            nama {{ $studentList->name }}
-        </li>
-        <li>
-            kelas {{ $studentList->class->name }}
-        </li>
-        <li>
-            @if ($studentList->gender == "male" || "Male")
-                laki-laki
-            @elseif ($studentList->gender == "female" || "Female")
-                perempuan
-            
-            @endif
-        </li>
-        <li>
-            walikelas {{ $studentList->class->homeroomTeacher->name }}
-        </li>
-        <li>
-            @foreach ($studentList->extracurriculars as $extracurricular)
-                {{ $extracurricular->name }}
-            @endforeach
-        </li>
-    </ol>
-
+    <h1 class="text-primary">{{ $studentList->name }}</h1>
+    <table class="table table-bordered">
+        <thead>
+            <th>no</th>
+            <th>nis</th>
+            <th>kelas</th>
+            <th>walikelas</th>
+            <th>extra</th>
+        </thead>
+        <tbody>
+            <td>1</td>
+            <td>{{ $studentList->nis }}</td>
+            <td>{{ $studentList->class->name }}</td>
+            <td>
+                {{ $studentList->class->homeroomTeacher->name }}
+            </td>
+            <td>
+                @foreach ($studentList->extracurriculars as $extracurricular)
+                    {{ $extracurricular->name }}
+                @endforeach
+            </td>
+        </tbody>
+    </table>
 @endsection
